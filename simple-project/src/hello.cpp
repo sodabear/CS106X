6912bd33d67ad7cf4857196221b12486cdd06319
James Jing <6886362+sodabear@users.noreply.github.com>
2024-02-29 17:38:47 +0800

    Step up QT in my local computer

---
+++ simple-project/src/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "console.h"
+#include "gwindow.h" // for GWindow
+#include "simpio.h"  // for getLine
+#include "vector.h"  // for Vector
+using namespace std;
+
+int main() {
+
+    cout << "Program completed." << endl;
+
+    return 0;
+}
